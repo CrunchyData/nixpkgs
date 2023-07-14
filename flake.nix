9d0f6e290da8bd1d5085d9ac4ad2c750d41991a4
Adam Brightwell <adam.brightwell@crunchydata.com>
2023-07-14 09:36:50 -0400

    Update ameba to v1.4.3.
    Update ameba from v1.4.2 -> v1.4.3.

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     crystal-src = { url = "github:crystal-lang/crystal/release/1.8"; flake = false; };
-    ameba-src = { url = "github:crystal-ameba/ameba/v1.4.2"; flake = false; };
+    ameba-src = { url = "github:crystal-ameba/ameba/v1.4.3"; flake = false; };
   };
 
   outputs = inputs@{ self, flake-utils, nixpkgs, ... }:
