cafeaff77ee844614ade43775f1b5767bc960bcb
Will Leinweber <will@bitfission.com>
2024-03-27 18:42:35 +0100

    disable static on x86 darwin

--- crystal/default.nix
+++ crystal/default.nix
@@ -1,13 +1,22 @@
 { pkgs, staticBoehmgc, system, inputs }:
 let
+
+  # NOTE: currently (2023-11-29) `nix flake check` fails on x86 macs due to
+  #    error: don't yet have a `targetPackages.darwin.LibsystemCross for x86_64-apple-darwin`
+  # so only have a static package on the other platforms for now.
+  # some maybe relevant issues:
+  # https://github.com/NixOS/nixpkgs/pull/256590
+  # https://github.com/NixOS/nixpkgs/issues/180771
+  # https://github.com/NixOS/nixpkgs/issues/270375
+  crystalStatic = if system == "x86_64-darwin" then null else "crystalStatic";
+
   packages = rec {
+    inherit (pkgs) shards crystal2nix ameba;
+
     base = pkgs.crystal;
 
     crystal = pkgs.callPackage ./crystalWrapped.nix { crystal = base; };
-
-    crystalStatic = pkgs.pkgsStatic.callPackage ./crystalWrapped.nix { crystal = base; boehmgc = staticBoehmgc; };
-
-    inherit (pkgs) shards crystal2nix ameba;
+    ${crystalStatic} = pkgs.pkgsStatic.callPackage ./crystalWrapped.nix { crystal = base; boehmgc = staticBoehmgc; };
   };
 
   simple_check = given_pkg: cmd:
